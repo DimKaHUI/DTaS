b1545f11d4bad80b62275a400971ab1081f7a608
DimKaHUI <wet75787@tqosi.com>
2017-10-29 16:55:53 +0300

    Added result for removing action

--- Lab_03/Lab_03/Lab_03/stacklist.h
+++ Lab_03/Lab_03/Lab_03/stacklist.h
@@ -9,6 +9,6 @@ typedef struct ListStack
 } ListStack;
 
 int liststack_add(ListStack *stack, int a);
-int liststack_remove(ListStack *stack);
+int liststack_remove(ListStack *stack, int *result);
 int liststack_is_empty(ListStack *stack);
 ListStack liststack_create();
