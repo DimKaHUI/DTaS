8aca576b34936b804580198963ff1045e8fece47
DimKaHUI <wet75787@tqosi.com>
2017-10-29 16:55:39 +0300

    Added result for removing action

--- Lab_03/Lab_03/Lab_03/stackarr.h
+++ Lab_03/Lab_03/Lab_03/stackarr.h
@@ -3,6 +3,10 @@
 // Array-based stack
 #define STACK_ARR_MAXSIZE 64
 
+// Error codes
+#define ERROR_STACK_ARR_OVERFLOW -0x11F
+#define ERROR_STACK_ARR_EMPTY -0x12F
+
 typedef struct ArrayStack
 {
 	int arr[STACK_ARR_MAXSIZE];
@@ -10,6 +14,6 @@ typedef struct ArrayStack
 } ArrayStack;
 
 int arrstack_add(ArrayStack *stack, int a);
-int arrstack_remove(ArrayStack *stack);
+int arrstack_remove(ArrayStack *stack, int *result);
 int arrstack_is_empty(ArrayStack *stack);
 ArrayStack arrstack_create();
