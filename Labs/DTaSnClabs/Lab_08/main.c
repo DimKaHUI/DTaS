e203f4fc0ad14e5fb7ceac226d44de3655fd0c27
DimKaHUI <wet75787@tqosi.com>
2017-12-24 20:32:01 +0300

    Algorithm implemented

--- Labs/DTaSnClabs/Lab_08/main.c
+++ Labs/DTaSnClabs/Lab_08/main.c
@@ -2,115 +2,115 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "locale.h"
+#include <conio.h>
 
-
-#define SIZE 6
-#define DIST_UNREACHABLE 10000
-
-int main()
+typedef struct graph
 {
-	int link_matrix[SIZE][SIZE]; // матрица св€зей
-	int distances[SIZE]; // минимальное рассто€ние
-	int visited[SIZE]; // посещенные вершины
-	int temp;
-	int minindex, min;
-	system("chcp 1251");
-	system("cls");
+	int **link_matrix;
+	int *visited;
+	int nodes_count;
+}graph;
+
+graph *setup_graph(int nodes_count)
+{
+	graph *graph = malloc(sizeof(graph));
+	graph->link_matrix = malloc(sizeof(float*));
+	for (int i = 0; i < nodes_count; i++)
+		graph->link_matrix[i] = malloc(sizeof(float) * nodes_count);
+	graph->visited = malloc(sizeof(int)* nodes_count);
 
 	// »нициализаци€ матрицы св€зей
-	for (int i = 0; i < SIZE; i++)
+	int temp;
+	for (int i = 0; i < nodes_count; i++)
 	{
-		link_matrix[i][i] = 0;
-		for (int j = i + 1; j < SIZE; j++)
+		for (int j = 0; j < nodes_count; j++)
 		{
-			printf("Input distance %d - %d: ", i + 1, j + 1);
+			if (i == j)
+			{
+				graph->link_matrix[i][j] = 0;
+				continue;
+			}
+			printf("Input reachability from %d to %d: ", i + 1, j + 1);
 			scanf("%d", &temp);
-			link_matrix[i][j] = temp;
-			link_matrix[j][i] = temp;
+			if (temp != 0)
+				graph->link_matrix[i][j] = 1;
+			else
+				graph->link_matrix[i][j] = 0;
 		}
 	}
 
-	// ¬ывод матрицы св€зей
-	for (int i = 0; i < SIZE; i++)
+	for (int i = 0; i < nodes_count; i++)
 	{
-		for (int j = 0; j < SIZE; j++)
-			printf("%5d ", link_matrix[i][j]);
+		for (int j = 0; j < nodes_count; j++)
+		{
+			printf("%d ", graph->link_matrix[i][j]);
+		}
 		printf("\n");
 	}
 
-	//»нициализаци€ вершин и рассто€ний
-	for (int i = 0; i < SIZE; i++)
+	for (int i = 0; i < nodes_count; i++)
+		graph->visited[i] = 0;
+	graph->nodes_count = nodes_count;
+	return graph;
+}
+
+void reset_visited(graph *graph)
+{
+	for (int i = 0; i < graph->nodes_count; i++)
+		graph->visited[i] = 1;
+}
+
+void Reach(graph *graph, int v)
+{
+	graph->visited[v] = 0;
+	for (int u = 0; u < graph->nodes_count; u++)
 	{
-		distances[i] = DIST_UNREACHABLE;
-		visited[i] = 1;
+		if (graph->link_matrix[v][u] != 0 && graph->visited[u])
+			Reach(graph, u);
 	}
+}
 
-	distances[0] = 0;
-
-	// Ўаг алгоритма
-	do 
+void Reachable(graph *graph, int v)
+{
+	reset_visited(graph);
+	printf("Nodes, not reachable from %d: ", v + 1);
+	Reach(graph, v);
+	for (int i = 0; i < graph->nodes_count; i++)
 	{
-		minindex = DIST_UNREACHABLE;
-		min = DIST_UNREACHABLE;
-		for (int i = 0; i < SIZE; i++)
-		{ // ≈сли вершину ещЄ не обошли и вес меньше min
-			if ((visited[i] == 1) && (distances[i] < min))
-			{ // ѕереприсваиваем значени€
-				min = distances[i];
-				minindex = i;
-			}
-		}
-		// ƒобавл€ем найденный минимальный вес
-		// к текущему весу вершины
-		// и сравниваем с текущим минимальным весом вершины
-		if (minindex != DIST_UNREACHABLE)
+		if (graph->visited[i] == 1 && i != v)
 		{
-			for (int i = 0; i<SIZE; i++)
-			{
-				if (link_matrix[minindex][i] > 0)
-				{
-					temp = min + link_matrix[minindex][i];
-					if (temp < distances[i])
-					{
-						distances[i] = temp;
-					}
-				}
-			}
-			visited[minindex] = 0;
-		}
-	} while (minindex < DIST_UNREACHABLE);
-
-	// ¬ывод кратчайших рассто€ний до вершин
-	printf("\nShortest ways to nodes: \n");
-	for (int i = 0; i<SIZE; i++)
-		printf("%5d ", distances[i]);
-
-	// ¬осстановление пути
-	int ver[SIZE]; // массив посещенных вершин
-	int end = 4; // индекс конечной вершины = 5 - 1
-	ver[0] = end + 1; // начальный элемент - конечна€ вершина
-	int k = 1; // индекс предыдущей вершины
-	int weight = distances[end]; // вес конечной вершины
-
-	while (end > 0) // пока не дошли до начальной вершины
-	{
-		for (int i = 0; i<SIZE; i++) // просматриваем все вершины
-		if (link_matrix[end][i] != 0)   // если св€зь есть
-		{
-			int temp = weight - link_matrix[end][i]; // определ€ем вес пути из предыдущей вершины
-			if (temp == distances[i]) // если вес совпал с рассчитанным
-			{                 // значит из этой вершины и был переход
-				weight = temp; // сохран€ем новый вес
-				end = i;       // сохран€ем предыдущую вершину
-				ver[k] = i + 1; // и записываем ее в массив
-				k++;
-			}
+			printf("%d ", i + 1);
 		}
 	}
-	// ¬ывод пути (начальна€ вершина оказалась в конце массива из k элементов)
-	printf("\nThe shortest way:\n");
-	for (int i = k - 1; i >= 0; i--)
-		printf("%3d ", ver[i]);
-	getchar();
-	return 0;
+	printf("\n");
+}
+
+int main()
+{
+	int size, node;
+	printf("Input amount of nodes: ");
+	if (scanf("%d", &size) != 1)
+	{
+		printf("Illegal symbol!\n");
+		return -1;
+	}
+	if (size <= 0)
+	{
+		printf("Count of nodes must be bigger than zero\n");
+		return -1;
+	}
+	printf("Input node number to find, which nodes are unreachable from it: ");
+	if (scanf("%d", &node) != 1)
+	{
+		printf("Illegal symbol!\n");
+		return -1;
+	}
+	if (node < 1 || node > size)
+	{
+		printf("Node number must be inside [1, %d]\n", size);
+		return -1;
+	}
+	struct graph *g = setup_graph(size);
+	Reachable(g, node - 1);
+	_getch();
 }
